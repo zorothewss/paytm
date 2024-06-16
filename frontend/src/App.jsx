7e2757285e51b98593390aae2a13978f9304e7c0
Yakshith15 <daggupatiyakshithnaidu@gmail.com>
2024-06-17 01:35:30 +0530

    frontend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
-// import { SendMoney } from "./pages/SendMoney";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path="/send" element={<SendMoney />} />  */}
+          <Route path="/send" element={<SendMoney />} /> 
         </Routes>
       </BrowserRouter>
     </>
